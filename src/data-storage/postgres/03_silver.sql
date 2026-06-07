78c456b06edac8aeaea95c0222be833371ff93d1
eng-alexis <alexispereira220@gmail.com>
2026-06-07 02:48:47 -0300

    feat - cria tabelas da camada silver

---
+++ src/data-storage/postgres/03_silver.sql
@@ -0,0 +1,30 @@
+-- Tabela silver.eventos
+
+CREATE TABLE IF NOT EXISTS silver.eventos(
+    evento_id   UUID,
+    evento_time TIMESTAMP,
+    emit_time   TIMESTAMP,
+    tipo_evento VARCHAR(20),
+    evento_seq  INTEGER,
+    id_loja     INTEGER,
+    id_caixa    INTEGER,
+    id_pedido   INTEGER,
+    produto_id  INTEGER,
+    quantidade  INTEGER,
+    valor_unitario NUMERIC(6,2)
+);
+
+-- Tabela silver.produtos
+
+CREATE TABLE IF NOT EXISTS silver.produtos(
+    produto_id INTEGER,
+    produto    VARCHAR(30),
+    valor      NUMERIC(6,2)
+);
+
+-- Tabela silver.lojas
+
+CREATE TABLE IF NOT EXISTS silver.loja(
+    loja_id INTEGER,
+    caixas  JSONB
+);
