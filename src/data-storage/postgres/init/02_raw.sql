822233bc616186870bd5faaa642ed6b2a3399691
eng-alexis <alexispereira220@gmail.com>
2026-06-07 19:13:28 -0300

    fix: corrige constraint da coluna na tabela raw

--- src/data-storage/postgres/init/02_raw.sql
+++ src/data-storage/postgres/init/02_raw.sql
@@ -1,35 +1,35 @@
 --Tabela raw.eventos
 
-CREATE TABLE raw.eventos(
+CREATE TABLE IF NOT EXISTS raw.eventos(
     id SERIAL PRIMARY KEY,
     arquivo_origem TEXT,
     dados JSONB,
-    data_ingestao TIMESTAMP DEFAUT CURRENT_TIMESTAMP
+    data_ingestao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 --Tabela raw.produtos
 
-CREATE TABLE raw.produtos(
+CREATE TABLE IF NOT EXISTS raw.produtos(
     id SERIAL PRIMARY KEY,
     arquivo_origem TEXT,
     dados JSONB,
-    data_ingestao TIMESTAMP DEFAUT CURRENT_TIMESTAMP
+    data_ingestao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 --Tabela raw.lojas
 
-CREATE TABLE raw.lojas(
+CREATE TABLE IF NOT EXISTS raw.lojas(
     id SERIAL PRIMARY KEY,
     arquivo_origem TEXT,
     dados JSONB,
-    data_ingestao TIMESTAMP DEFAUT CURRENT_TIMESTAMP
+    data_ingestao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 --Tabela raw.quarantine
 
-CREATE TABLE raw.quarantine(
+CREATE TABLE IF NOT EXISTS raw.quarantine(
     id SERIAL PRIMARY KEY,
     arquivo_origem TEXT,
     dados JSONB,
-    data_ingestao TIMESTAMP DEFAUT CURRENT_TIMESTAMP
+    data_ingestao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
