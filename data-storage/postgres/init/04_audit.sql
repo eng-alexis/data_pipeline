3089f417d435240d4c2cb63f3cfc8de87e10c042
eng-alexis <alexispereira220@gmail.com>
2026-06-08 17:03:54 -0300

    feat - cria tabelas da camada audit

---
+++ data-storage/postgres/init/04_audit.sql
@@ -0,0 +1,57 @@
+CREATE TABLE IF NOT EXISTS audit.pipeline_step(
+    id_step BIGSERIAL PRIMARY KEY,
+    id_exec BIGINT,
+    id_arquivo BIGINT,
+    camada VARCHAR(15),
+    entidade VARCHAR(30),
+    linhas_lidas INTEGER,
+    linhas_gravadas INTEGER,
+    inicio TIMESTAMP,
+    fim TIMESTAMP,
+    duracao_ms BIGINT,
+    status VARCHAR(15),
+
+    CONSTRAINT fk_id_exec
+    FOREIGN KEY (id_exec)
+    REFERENCES audit.pipeline_execution(id_exec),
+
+    CONSTRAINT fk_file_name 
+    FOREIGN KEY (id_arquivo)
+    REFERENCES audit.file_history(id_arquivo)
+);
+
+CREATE TABLE IF NOT EXISTS audit.pipeline_execution(
+    id_exec BIGINT PRIMARY KEY,
+    inicio TIMESTAMP,
+    fim TIMESTAMP,
+    status VARCHAR(15),
+
+    CONSTRAINT ck_status
+    CHECK ( status IN (
+        'PENDENTE',
+        'PROCESSANDO',
+        'SUCESSO',
+        'ERRO')
+);
+
+CREATE TABLE IF NOT EXISTS audit.file_history(
+    id_arquivo BIGSERIAL PRIMARY KEY,
+    hash VARCHAR(64),
+    nome_arquivo VARCHAR(100),
+    data_ingestao TIMESTAMP,
+    id_exec BIGINT,
+    status VARCHAR(15),
+
+    CONSTRAINT fk_id_exec 
+    FOREIGN KEY (id_exec)
+    REFERENCES audit.pipeline_execution(id_exec),
+
+    CONSTRAINT uq_hash 
+    UNIQUE(hash),
+
+    CONSTRAINT ck_status
+    CHECK ( status IN (
+        'PROCESSADO',
+        'ERRO')
+);
+
