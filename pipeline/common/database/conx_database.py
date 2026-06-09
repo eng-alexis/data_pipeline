4ff60c7b11a14998f710aec814e0b206ba04a8c9
eng-alexis <alexispereira220@gmail.com>
2026-06-08 22:45:15 -0300

    feat: cria conexão com o banco de dados postgres

---
+++ pipeline/common/database/conx_database.py
@@ -0,0 +1,19 @@
+import psycopg2
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_db_connection ():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port= os.getenv("POSTGRES_PORT"),
+        database= os.getenv("POSTGRES_DB"),
+        user= os.getenv("POSTGRES_USER"),
+        password= os.getenv("POSTGRES_PASSWORD")
+    )
+
+    cursor = conn.cursor()
+ 
+    return cursor
