a2776fb485b00b5507d92253aca88a67aa6b180c
eng-alexis <alexispereira220@gmail.com>
2026-06-10 18:10:52 -0300

    feat: extrai o registro e o nome do arquivo jsonl

--- pipeline/src/raw/extract/extract_json.py
+++ pipeline/src/raw/extract/extract_json.py
@@ -1,5 +1,5 @@
 # Encontra arquivos jsonl no caminho especificado pelo usuário
-
+import json
 from pathlib import Path
 
 def encontrar_arquivos(caminho):
@@ -13,3 +13,19 @@ def encontrar_arquivos(caminho):
         arquivos_encontrados.append(arquivo)
 
     return arquivos_encontrados
+
+# Extrair registro e nome do arquivo jsonl
+
+def extrair_registros(lista_arquivos):
+
+    for arquivo in lista_arquivos:
+
+        with open(arquivo, 'r', encoding="utf-8") as arq_json:
+
+            for linha in arq_json:
+
+                linha = linha.strip()
+
+                if linha:
+
+                    yield Path(arquivo).name, json.loads(linha)
