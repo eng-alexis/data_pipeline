928a3294c2d58965e32c72bd817b2b6f23534c56
eng-alexis <alexispereira220@gmail.com>
2026-06-12 17:54:45 -0300

    feat: adiciona funções para manipulação e registro de arquivos

--- pipeline/src/raw/file_control/move_files.py
+++ pipeline/src/raw/file_control/move_files.py
@@ -1,8 +1,36 @@
-# Move arquivos processados
-
 import shutil
+
+# Registra novo arquivo no banco
+
+def reg_novo_arquivo(connection_db, hash, nome_arquivo, tamanho_bytes, id_exec):
+
+    conx = connection_db
+
+    cursor = conx.cursor()
+
+    query= """INSERT INTO audit.file_history(
+                hash, nome_arquivo, tamanho_bytes,
+                data_ingestao, id_exec, status)
+                VALUES(%s, %s, %s, CURRENT_DATE, %s, 'PROCESSADO')"""
+    
+    valores = (hash, nome_arquivo, tamanho_bytes, id_exec)
+
+    try:
+
+        cursor.execute(query, valores)
+        conx.commit()
+
+        return True
+
+    except Exception as e:
+        print(f"Erro ao registrar file: {nome_arquivo} - {e}")
+
+
+# Move arquivos processados
 
 def move_file(arquivo, destino):
 
     shutil.move(arquivo,destino)
+    
+    return True
 
