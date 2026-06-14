e881d6129068c3ab0fa5d5e079efcd162e2e7c76
eng-alexis <alexispereira220@gmail.com>
2026-06-14 19:16:33 -0300

    feat: registra informações do step na tabela audit.pipeline_step

---
+++ pipeline/common/monitoring/pipeline_step.py
@@ -0,0 +1,25 @@
+# Registra informações do step do pipeline
+
+def reg_new_step(conection_bd, id_exec, id_arquivo, camada, entidade, linhas_lidas, linhas_gravadas, inicio, fim, duracao):
+
+    conx = conection_bd
+    cursor = conx.cursor()
+
+    query = """INSERT INTO audit.pipeline_step(
+                    id_exec,
+                    id_arquivo,
+                    camada,
+                    entidade,
+                    linhas_lidas,
+                    linhas_gravadas,
+                    inicio,
+                    fim,
+                    duracao_ms,
+                    status)
+                VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """
+    valores = (id_exec, id_arquivo, camada, entidade, linhas_lidas, linhas_gravadas, inicio, fim, duracao,'SUCESSO')
+
+    cursor.execute(query, valores)
+
+    conx.commit()
