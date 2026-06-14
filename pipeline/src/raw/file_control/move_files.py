ac2d6ce80064d28212cf3a17335d74bc24ec892c
eng-alexis <alexispereira220@gmail.com>
2026-06-14 19:15:29 -0300

    fix: corrige query de insert

--- pipeline/src/raw/file_control/move_files.py
+++ pipeline/src/raw/file_control/move_files.py
@@ -11,16 +11,19 @@ def reg_novo_arquivo(connection_db, hash, nome_arquivo, data_ingestao, tamanho_b
     query= """INSERT INTO audit.file_history(
                 hash, nome_arquivo, tamanho_bytes,
                 data_ingestao, id_exec, status)
-                VALUES(%s, %s, %s, %s, %s, 'PROCESSADO')"""
+                VALUES(%s, %s, %s, %s, %s, 'PROCESSADO')
+                RETURNING id_arquivo"""
     
     valores = (hash, nome_arquivo, tamanho_bytes, data_ingestao, id_exec)
 
     try:
 
         cursor.execute(query, valores)
+        file_id = cursor.fetchone()[0]
+
         conx.commit()
 
-        return True
+        return file_id
 
     except Exception as e:
         print(f"Erro ao registrar file: {nome_arquivo} - {e}")
