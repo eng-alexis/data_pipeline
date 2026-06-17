8a6a0b80c12e8b31c77812b0671542a74a80917a
eng-alexis <alexispereira220@gmail.com>
2026-06-17 03:09:21 -0300

    feat: adiciona funções para carregamento de eventos na silver.eventos

---
+++ pipeline/src/silver/load/load_to_silver.py
@@ -0,0 +1,43 @@
+# Descobrir ultimo id processado (watermark)
+from pipeline.common.database.conx_database import get_db_connection
+
+conx = get_db_connection()
+
+def get_ultimo_id(conection_db, watermark_name):
+    
+    conx = conection_db
+    cursor = conx.cursor()
+
+    query = """SELECT COALESCE(ultimo_id, 0) FROM audit.pipeline_watermark
+                WHERE watermark_name = %s;"""
+    
+    cursor.execute(query,(watermark_name,))
+
+    return cursor.fetchone()[0]
+
+# Realizar extração
+
+def load_to_silver(conection_db, ultimo_id, script_sql):
+
+    conx = conection_db
+    cursor = conx.cursor()
+    id_anterior = ultimo_id
+
+    sql_inject_file = script_sql
+
+    with open(sql_inject_file,'r', encoding='utf-8') as sql:
+        comando = sql.read()
+
+        cursor.execute(comando,(ultimo_id,))
+        linhas_lidas = cursor.rowcount
+
+        linhas_load = """SELECT COUNT(*) FROM silver.eventos
+                                WHERE id_raw > %s;"""
+        
+        cursor.execute(linhas_load,(ultimo_id,))
+        linhas_gravadas = cursor.fetchone()[0]
+
+        conx.commit()
+
+    return linhas_lidas, id_anterior, linhas_gravadas
+
---
+++ pipeline/src/silver/load/sql/load_to_silver.sql
@@ -0,0 +1,53 @@
+MERGE INTO silver.eventos s USING(
+            SELECT
+            (dados->>'event_id')::UUID AS evento_id,
+            (dados->>'event_time')::TIMESTAMP AS evento_time,
+            (dados->>'emit_time')::TIMESTAMP AS emit_time,
+            (dados->>'tipo_evento')::VARCHAR(20) AS tipo_evento,
+            (dados->>'evento_seq')::INTEGER AS evento_seq,
+            (dados->>'id_loja')::INTEGER AS id_loja,
+            (dados->>'id_caixa')::INTEGER AS id_caixa,
+            (dados->>'id_pedido')::INTEGER AS id_pedido,
+            (dados->>'produto_id')::INTEGER AS produto_id,
+            (dados->>'quantidade')::INTEGER AS quantidade,
+            (dados->>'valor_unitario')::NUMERIC(6,2) AS valor_unitario,
+            (id_raw)::BIGINT AS id_raw,
+            (data_ingestao_raw)::TIMESTAMP AS data_ingestao_raw
+        FROM 
+            raw.eventos
+        WHERE
+            id_raw > %s) r
+
+            ON (r.evento_id = s.evento_id)
+
+        WHEN NOT MATCHED THEN
+
+        INSERT(
+            evento_id,
+            evento_time,
+            emit_time,
+            tipo_evento,
+            evento_seq,
+            id_loja,
+            id_caixa,
+            id_pedido,
+            produto_id,
+            quantidade,
+            valor_unitario,
+            id_raw,
+            data_ingestao_raw) 
+
+        VALUES(
+            r.evento_id,
+            r.evento_time,
+            r.emit_time,
+            r.tipo_evento,
+            r.evento_seq,
+            r.id_loja,
+            r.id_caixa,
+            r.id_pedido,
+            r.produto_id,
+            r.quantidade,
+            r.valor_unitario,
+            r.id_raw,
+            r.data_ingestao_raw);
