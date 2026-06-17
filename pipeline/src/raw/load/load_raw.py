2d89b8d12cd512f0fec781345f0ff7cedeedc4f8
eng-alexis <alexispereira220@gmail.com>
2026-06-17 17:23:54 -0300

    feat: valida schema de cada registro antes do load na raw.eventos

--- pipeline/src/raw/load/load_raw.py
+++ pipeline/src/raw/load/load_raw.py
@@ -1,19 +1,25 @@
 from psycopg2.extras import Json
 
+from pipeline.src.raw.validate.schema_validate import schema_validation
+
+
+
 # Carrega dados do jsonl na banco RAW
 
 def load_to_raw(conx, nome_arquivo, lote):
 
     cursor = conx.cursor()
-    
+
     try:
 
         for linha in lote:
 
-            query = """INSERT INTO raw.eventos(arquivo_origem, dados)
-                            VALUES(%s, %s);"""
+            status, motivo = schema_validation(linha)
 
-            valores = (nome_arquivo, Json(linha))
+            query = """INSERT INTO raw.eventos(arquivo_origem, dados, schema_version, schema_status, schema_error)
+                            VALUES(%s, %s, %s, %s, %s);"""
+
+            valores = (nome_arquivo, Json(linha), 'V1', status, motivo)
 
             cursor.execute(query,valores)
 
