ca778bae55d61f4ad09055232dc5d909075e1e18
eng-alexis <alexispereira220@gmail.com>
2026-06-17 03:08:14 -0300

    feat: tranforma scripts raw_main e silver_main em funções para execução em sequência via orquestrador

--- pipeline/src/raw/raw_main.py
+++ pipeline/src/raw/raw_main.py
@@ -1,11 +1,11 @@
-# Importa funções
+    # Importa funções
 
 from pipeline.common.database.conx_database import get_db_connection
 from pipeline.common.monitoring.pipeline_exec import reg_new_execution, update_execution
 from pipeline.common.monitoring.pipeline_step import reg_new_step
-from pipeline.common.context.pipeline_context import gerar_id_contexto, upd_watermark_exec
+from pipeline.common.context.pipeline_context import upd_watermark_exec
 
-from pipeline.src.raw.extract.extract_json import encontrar_arquivos, extrair_registros
+from pipeline.src.raw.extract.extract_json import extrair_registros
 from pipeline.src.raw.file_control.search_file_hist import gerar_hash, find_hash
 from pipeline.src.raw.file_control.move_files import reg_novo_arquivo, move_file
 from pipeline.src.raw.load.load_raw import load_to_raw
@@ -13,111 +13,115 @@ from pipeline.src.raw.load.load_raw import load_to_raw
 from pathlib import Path
 from datetime import datetime
 
-# Diretorios e utilitarios
-
-path_padrão     = "/home/alexis/data_pipeline/pipeline/testes/pos/"
 path_processado = "/home/alexis/data_pipeline/pipeline/testes/arq_processados/"
 
+    # Diretorios e utilitarios
+
 conexao_bd = get_db_connection()
-cursor     = conexao_bd.cursor()
 
-jsonl_arquivo = encontrar_arquivos(path_padrão)
+def etapa_raw(id_execution, json_file):
 
-for arquivo in jsonl_arquivo:
+    try:
 
-# Inicia Pipeline
+    # Inicia Pipeline
 
-    id_exec = gerar_id_contexto(conexao_bd)
+        id_exec = id_execution
+        arquivo = json_file
+        inicio_pipeline = datetime.now()
 
-# extrai informações 
+    # extrai informações 
 
-    inicio        = datetime.now()
-    hash_arquivo  = gerar_hash(arquivo)
-    nome_arquivo  = Path(arquivo).name
-    tamanho_bytes = arquivo.stat().st_size
-    entidade      = 'raw.eventos'
+        hash_arquivo    = gerar_hash(arquivo)
+        nome_arquivo    = Path(arquivo).name
+        tamanho_bytes   = arquivo.stat().st_size
 
-# Registra nova execução do pipeline na tabela de auditoria
+        entidade        = "raw.eventos"
 
-    new_exec = reg_new_execution(conexao_bd, id_exec, inicio, nome_arquivo, hash_arquivo)
-    conexao_bd.commit()
+    # Registra nova execução do pipeline na tabela de auditoria
 
-# Valida arquivo
-
-    sh_file  = find_hash(conexao_bd, hash_arquivo)
-    
-# Aualiza tabela de auditoria, caso hash já exista no banco
-
-    if sh_file:
-
-        fim = datetime.now()
-        upd_exec = update_execution(conexao_bd, id_exec, fim, "DUPLICADO", "Hash já processado anteriormente")
-        upd_id_exec = upd_watermark_exec(conexao_bd, id_exec, fim)
-        conexao_bd.commit()
-        
-        print(f"Arquivo {nome_arquivo} já foi processado anteriormente")
-
-# Registra novo arquivo na tabela de auditoria.
-
-    else:
-
-        data_ingestao = datetime.now()
-        
-        new_file = reg_novo_arquivo(conexao_bd, hash_arquivo, nome_arquivo, data_ingestao, tamanho_bytes, id_exec)
+        new_exec = reg_new_execution(conexao_bd, id_exec, inicio_pipeline, nome_arquivo, hash_arquivo)
         conexao_bd.commit()
 
-        if new_file:
+    # Valida arquivo
+
+        sh_file  = find_hash(conexao_bd, hash_arquivo)
+        
+    # Aualiza tabela de auditoria, caso hash já exista no banco
+
+        if sh_file:
+
+            fim = datetime.now()
+
+            upd_exec    = update_execution(conexao_bd, id_exec, fim, "DUPLICADO", "Hash já processado anteriormente")
+            upd_id_exec = upd_watermark_exec(conexao_bd, id_exec, fim)
+            conexao_bd.commit()
+            print("Alerta !")
+            print(f"Arquivo duplicado")
+            print("Pipeline Interrompido")
+            print("------------------")
+
+    # Registra novo arquivo na tabela de auditoria.
+
+        else:
+
+            data_ingestao = datetime.now()
             
-# Extrai registros e cria Lote.
+            new_file = reg_novo_arquivo(conexao_bd, hash_arquivo, nome_arquivo, data_ingestao, tamanho_bytes, id_exec)
+            conexao_bd.commit()
 
-            registros = extrair_registros(arquivo)
-
-            lote = []
-            tam_lote = 1000
-            qtd_lote = 0
-            linhas_lidas = 0
-            linhas_gravadas = 0
-            
-            for linha in registros:
-                linhas_lidas += 1
-
-                lote.append(linha)
+            if new_file:
                 
-# Carrega lote na tabela raw.
+    # Extrai registros e cria Lote.
 
-                if len(lote) == tam_lote:
+                registros = extrair_registros(arquivo)
+
+                lote = []
+                tam_lote = 1000
+                qtd_lote = 0
+                linhas_lidas = 0
+                linhas_gravadas = 0
+                
+                for linha in registros:
+                    linhas_lidas += 1
+
+                    lote.append(linha)
                     
+    # Carrega lote na tabela raw.
+
+                    if len(lote) == tam_lote:
+                        
+                        load = load_to_raw(conexao_bd, nome_arquivo, lote)
+                        conexao_bd.commit()
+                        linhas_gravadas += len(lote)
+                        lote.clear()
+                        qtd_lote += 1
+                
+                if lote:
+
                     load = load_to_raw(conexao_bd, nome_arquivo, lote)
                     conexao_bd.commit()
                     linhas_gravadas += len(lote)
-                    lote.clear()
                     qtd_lote += 1
-            
-            if lote:
+                    lote.clear()
 
-                load = load_to_raw(conexao_bd, nome_arquivo, lote)
-                conexao_bd.commit()
-                linhas_gravadas += len(lote)
-                qtd_lote += 1
-                lote.clear()
+                if load:
 
-            if load:
+    # Move arquivos processados e finaliza a etapa de ingestão.
 
-# Move arquivos processados e finaliza a etapa de ingestão.
+                    move = move_file(arquivo, path_processado)
 
-                move = move_file(arquivo, path_processado)
+                    if move:
 
-                if move:
+                        fim = datetime.now()
+                        diferenca = (fim - inicio_pipeline)
+                        duracao   = diferenca.total_seconds()
 
-                    fim = datetime.now()
+    # Atualiza tabela audit.pipeline_step
 
-                    update_execution(conexao_bd, id_exec, fim, 'SUCESSO','')
-                    upd_watermark_exec(conexao_bd, id_exec, fim)
-                    conexao_bd.commit()
+                        reg_new_step(conexao_bd, id_exec, new_file, 'RAW', entidade, linhas_lidas, linhas_gravadas, inicio_pipeline, fim, duracao)
+                        conexao_bd.commit()
 
-                    diferenca = (fim - inicio)
-                    duracao   = diferenca.total_seconds()
+                        return new_file, inicio_pipeline
 
-# Atualiza tabela audit.pipeline_step
-
-                    reg_new_step(conexao_bd, id_exec, new_file, 'RAW', entidade, linhas_lidas, linhas_gravadas, inicio, fim, duracao)
+    except Exception as e:
+        print(f"Erro na etapa raw: {e}")
---
+++ pipeline/src/silver/silver_main.py
@@ -0,0 +1,63 @@
+
+from pipeline.src.silver.load.load_to_silver import get_ultimo_id, load_to_silver
+
+from pipeline.common.context.pipeline_context import gerar_id_contexto, upd_watermark_exec, upd_watermark_layer
+from pipeline.common.database.conx_database import get_db_connection
+
+from pipeline.common.monitoring.pipeline_exec import update_execution
+from pipeline.common.monitoring.pipeline_step import reg_new_step
+
+from datetime import datetime
+
+def etapa_silver(id_execution, id_arquivo, inicio_pipeline):
+
+    try:
+        conexao_db = get_db_connection()
+        id_exec = id_execution
+        inicio_step = datetime.now()
+
+        # Carrega dados RAW -> SILVER
+
+        sql_path = '/home/alexis/data_pipeline/pipeline/src/silver/load/sql/load_to_silver.sql'
+
+        ultimo_id = get_ultimo_id(conexao_db, 'raw.eventos_last_id')
+
+        load = load_to_silver(conexao_db, ultimo_id, sql_path)
+
+        linhas_lidas, _, linhas_gravadas = load
+
+        # Atualiza id_contexto
+
+        fim = datetime.now()
+        upd_watermark_exec(conexao_db, id_exec, fim)
+
+        # Atualiza o ultimo id da entidade raw (que já foi processado para silver)
+
+        watermark = 'raw.eventos_last_id'
+        fim = datetime.now()
+
+        upd_watermark_layer(conexao_db, 'raw.eventos', watermark, fim)
+
+        # Atualizar execução do pipeline
+
+        fim = datetime.now()
+        status = 'SUCESSO'
+        msg = ''
+
+        update_execution(conexao_db, id_exec, fim, status, msg)
+
+        # Registra e Atualiza step silver
+
+        camada = 'SILVER'
+        entidade = 'silver.eventos'
+        fim = datetime.now()
+
+        diferenca = (fim - inicio_pipeline)
+        duracao = diferenca.total_seconds()
+        
+        reg_new_step(conexao_db, id_exec, id_arquivo, camada, entidade, linhas_lidas, linhas_gravadas, inicio_step, fim, duracao)
+
+        return True
+
+    except Exception as e:
+        print(f"Erro na etapa silver: {e}")
