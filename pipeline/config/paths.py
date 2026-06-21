6a911858cc5a5786ffcd3d50fb4da1418a7f30df
eng-alexis <alexispereira220@gmail.com>
2026-06-21 16:21:59 -0300

    feat: adiciona caminhos dos scripts sql de load das tabelas silver.produtos e silver.lojas

--- pipeline/config/paths.py
+++ pipeline/config/paths.py
@@ -5,10 +5,12 @@ from pathlib import Path
 PIPELINE_ROOT = Path(__file__).resolve().parents[1]
 PROJECT_ROOT  = Path(__file__).resolve().parents[2]
 
-JSON_EVENTS_DIR    = PROJECT_ROOT / "pdv_simulator" / "pdv_sales" / "sales"
+JSON_EVENTS_DIR    = PROJECT_ROOT / "pdv_simulator" / "pdv_sales" 
 JSON_DATABASE_DIR  = PROJECT_ROOT / "pdv_simulator" / "pdv_sales" / "database"
 
 JSON_PROCESSED_DIR = PIPELINE_ROOT / "processed_files" / "arq_processados"
 JSON_DUPLICATE_DIR = PIPELINE_ROOT / "processed_files" / "arq_duplicados"
 
-SQL_SCRIPT_LOAD    = PIPELINE_ROOT / "src" / "silver" / "load" / "sql" / "load_to_silver.sql"
+SQL_LOAD_TO_EVENTOS  = PIPELINE_ROOT / "src" / "silver" / "load" / "sql" / "load_to_eventos.sql"
+SQL_LOAD_TO_PRODUTOS = PIPELINE_ROOT / "src" / "silver" / "load" / "sql" / "load_to_produtos.sql"
+SQL_LOAD_TO_LOJAS    = PIPELINE_ROOT / "src" / "silver" / "load" / "sql" / "load_to_lojas.sql"
