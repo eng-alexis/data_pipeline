c074fc8f1d1131a004a2da66b5757e7f614b0612
eng-alexis <alexispereira220@gmail.com>
2026-06-21 16:16:24 -0300

    fix: adiciona colunas ausentes nas tabelas silver.produtos e silver.lojas

--- data_storage/postgres/init/03_silver.sql
+++ data_storage/postgres/init/03_silver.sql
@@ -1,7 +1,7 @@
 -- Tabela silver.eventos
 
 CREATE TABLE IF NOT EXISTS silver.eventos(
-    evento_id   UUID,
+    id_evento   UUID,
     evento_time TIMESTAMP,
     emit_time   TIMESTAMP,
     tipo_evento VARCHAR(20),
@@ -19,14 +19,24 @@ CREATE TABLE IF NOT EXISTS silver.eventos(
 -- Tabela silver.produtos
 
 CREATE TABLE IF NOT EXISTS silver.produtos(
-    produto_id INTEGER,
+    id_produto INTEGER,
     produto    VARCHAR(30),
-    valor      NUMERIC(6,2)
+    valor      NUMERIC(6,2),
+    id_raw BIGINT,
+    data_ingestao_raw TIMESTAMP
 );
 
 -- Tabela silver.lojas
 
-CREATE TABLE IF NOT EXISTS silver.loja(
-    loja_id INTEGER,
-    caixas  JSONB
+CREATE TABLE IF NOT EXISTS silver.lojas(
+    id_loja INTEGER,
+    cnpj VARCHAR(20),
+    endereco VARCHAR(60),
+    bairro VARCHAR(30),
+    cidade VARCHAR(30),
+    estado VARCHAR(30),
+    gerente VARCHAR(60),
+    horario_atendimento VARCHAR(30),
+    id_raw BIGINT,
+    data_ingestao_raw TIMESTAMP
 );
