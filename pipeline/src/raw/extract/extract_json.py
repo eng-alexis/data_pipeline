94fe3e482742abb8a6c2105efe4204f2fdd4d6eb
eng-alexis <alexispereira220@gmail.com>
2026-06-21 16:30:47 -0300

    feat: permite a extração de registros de 2 tipos de arquivos (json e jsonl)

--- pipeline/src/raw/extract/extract_json.py
+++ pipeline/src/raw/extract/extract_json.py
@@ -1,25 +1,39 @@
 # Encontra arquivos jsonl no caminho especificado pelo usuário
+
 import json
 from pathlib import Path
 
-def encontrar_arquivos(caminho):
+def encontrar_arquivos(caminho, formato):
 
     path = Path(caminho)
-    arquivos_jsonl = list(path.rglob(f"*.jsonl"))
+    arquivos_jsonl = list(path.rglob(f"*.{formato}"))
     
     for arquivo in arquivos_jsonl:
 
         if arquivo:
             yield arquivo
 
-# Extrair registro, pasta de origem e nome do arquivo jsonl
+# Extrair registros de arquivos jsonl e json
 
-def extrair_registros(arquivo):
+def extrair_registros(arquivo, tipo_file):
 
     with open(arquivo, 'r', encoding="utf-8") as arq_json:
 
-        for linha in arq_json:
-            linha = linha.strip()
+        if tipo_file == "jsonl":
 
-            if linha:
-                yield json.loads(linha)
+            for linha in arq_json:
+                linha = linha.strip()
+
+                if linha:
+                    yield json.loads(linha)
+
+        elif tipo_file == "json":
+
+            registros = json.load(arq_json)
+
+            for linha in registros:
+
+                yield linha
+
+        else:
+            print("Tipo de arquivo não é permitido")
