67da2ff987ec3f55045546a4304bf25ce3fa9343
eng-alexis <alexispereira220@gmail.com>
2026-06-21 16:11:57 -0300

    feat: adiciona colunas para registro da entidade e schema_status nas tabelas raw

--- data_storage/postgres/init/02_raw.sql
+++ data_storage/postgres/init/02_raw.sql
@@ -4,28 +4,11 @@ CREATE TABLE IF NOT EXISTS raw.eventos(
     id_raw SERIAL PRIMARY KEY,
     arquivo_origem TEXT,
     dados JSONB,
+    entidade VARCHAR(20),
     data_ingestao_raw TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    schema_status VARCHAR(20),
     schema_version VARCHAR(20),
+    schema_status VARCHAR(20),
     observacao TEXT
-);
-
---Tabela raw.produtos
-
-CREATE TABLE IF NOT EXISTS raw.produtos(
-    id_raw SERIAL PRIMARY KEY,
-    arquivo_origem TEXT,
-    dados JSONB,
-    data_ingestao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
---Tabela raw.lojas
-
-CREATE TABLE IF NOT EXISTS raw.lojas(
-    id_raw SERIAL PRIMARY KEY,
-    arquivo_origem TEXT,
-    dados JSONB,
-    data_ingestao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 --Tabela raw.quarantine
@@ -34,8 +17,9 @@ CREATE TABLE IF NOT EXISTS raw.quarantine(
     id_raw SERIAL PRIMARY KEY,
     arquivo_origem TEXT,
     dados JSONB,
+    entidade VARCHAR(20),
     data_ingestao_raw TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    schema_status VARCHAR(20),
     schema_version VARCHAR(20),
+    schema_status VARCHAR(20),
     schema_error TEXT
 );
