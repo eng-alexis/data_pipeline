b2722c495a093872d935c0ac0afa29a01d3d5188
eng-alexis <alexispereira220@gmail.com>
2026-06-29 20:11:44 -0300

    fix: substitui comando sql fixo por comando que aceita parametros dentro do modulo

--- pipeline/common/context/pipeline_context.py
+++ pipeline/common/context/pipeline_context.py
@@ -40,18 +40,33 @@ def upd_watermark_exec(conection_db, id_exec_atual, data_execução):
 
     conx.commit()
 
+# Retorna o ultimo id processado pela etapa
+
+def get_ultimo_id(conection_db, watermark_name):
+    
+    conx = conection_db
+    cursor = conx.cursor()
+
+    query = """SELECT COALESCE(ultimo_id, 0) FROM audit.pipeline_watermark
+                WHERE watermark_name = %s;"""
+    
+    cursor.execute(query,(watermark_name,))
+
+    return cursor.fetchone()[0]
+
+
 # Atualiza ultimo id armazenado na entidade/tabela
 
-def upd_watermark_layer(conection_bd, watermark_name, data_execução):
+def upd_watermark_layer(conection_bd, column_name, table_name, watermark_name, data_execução):
 
     conx = conection_bd
     cursor = conx.cursor()
 
     # Busca o ultimo id da entidade (ex: id 100)
 
-    query_1 = f"""SELECT id_raw FROM raw.eventos
-                    ORDER by id_raw DESC"""
-    
+    query_1 = f"""SELECT MAX({column_name}) FROM {table_name};"""
+    # valores = (id, entidade)
+
     cursor.execute(query_1)
     ultimo_id = cursor.fetchone()[0]
 
@@ -63,7 +78,6 @@ def upd_watermark_layer(conection_bd, watermark_name, data_execução):
     
     valores = (ultimo_id, data_execução, watermark_name)
     
-    cursor.execute(query_2, valores)
     cursor.execute(query_2, valores)
 
     conx.commit()
