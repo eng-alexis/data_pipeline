f1cf210b03ad190c35f0e7aafb79bc18d83dfea6
eng-alexis <alexispereira220@gmail.com>
2026-07-05 21:48:59 -0300

    feat: adiciona input para coletar datas para o pdv_simulator

--- pdv_simulator/config/context.py
+++ pdv_simulator/config/context.py
@@ -1,10 +1,25 @@
 import pandas as pd
+from datetime import datetime
 
-inicio = '' # ex('2026-01-01')
-fim    = '' # ex('2026-01-02')
+# Input que recebe e valida datas de inicio e fim para o simulador
+
+data_inicio = input("digite a data inicial (ex: 2026-01-01): ")
+data_final = input("digite a data final (ex: 2026-01-01): ")
+
+try:
+    data_inicio_valida = datetime.strptime(data_inicio, "%Y-%m-%d").date()
+    data_inicio_valida = datetime.strptime(data_final, "%Y-%m-%d").date()
+
+except ValueError:
+    print("Erro: Data inválida! Use estritamente o formato AAAA-MM-DD")
+
+if data_inicio > data_final:
+    print("Erro: Data inválida! A data inicial deve ser menor que a data final")
+    raise 
 
 # Gera o range de datas diretamente
-DATA = pd.date_range(start=inicio, end=fim).strftime('%Y-%m-%d').tolist()
+
+DATA = pd.date_range(start=data_inicio, end=data_final).strftime('%Y-%m-%d').tolist()
 
 LOJAS = [1]
 
@@ -13,18 +28,18 @@ CAIXAS_POR_LOJA = {
 }
 
 PERFIL_HORA = {
-    8: 10,
-    9: 20,
-    10: 30,
-    11: 40,
-    12: 60,
-    13: 50,
-    14: 30, 
-    15: 25,
-    16: 30,
-    17: 40,
-    18: 70,
-    19: 80,
-    20: 60,
-    21: 30
+    8: 3,
+    9: 4,
+    10: 6,
+    11: 10,
+    12: 11,
+    13: 9,
+    14: 10, 
+    15: 6,
+    16: 7,
+    17: 9,
+    18: 12,
+    19: 17,
+    20: 12,
+    21: 4
 }
