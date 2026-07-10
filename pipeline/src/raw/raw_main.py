00a75dd583873008098780b05ebe118b6bee5d42
eng-alexis <alexispereira220@gmail.com>
2026-07-09 22:47:38 -0300

    feat: adiciona exceptions personalizadas no orquestrador raw_main

--- pipeline/src/raw/raw_main.py
+++ pipeline/src/raw/raw_main.py
@@ -1,109 +1,110 @@
 from pipeline.common.database.conx_database import get_db_connection
-from pipeline.common.monitoring.pipeline_exec import update_execution
-from pipeline.common.monitoring.pipeline_step import reg_new_step
-from pipeline.common.context.pipeline_context import upd_watermark_exec
-from pipeline.src.raw.extract.extract_json import extrair_registros, descobrir_entidade
-from pipeline.src.raw.validate.schema_validate import schema_event_validation, schema_product_validation, schema_store_validation
+from pipeline.common.monitoring.pipeline_step import reg_new_step, upd_step
+from pipeline.src.raw.extract.extract_json import extrair_registros,  is_file_empty, descobrir_entidade
 from pipeline.src.raw.validate.validate_main import validate_schema
 from pipeline.src.raw.file_control.hash import gerar_hash, find_hash
-from pipeline.src.raw.file_control.manipulate_file import rename_file, move_file, delete_empty_dir
+from pipeline.src.raw.file_control.manipulate_file import file_new_name, rename_file, move_file, delete_empty_dir
 from pipeline.src.raw.file_control.register_file import reg_new_file
 from pipeline.src.raw.load.load_raw import load_to_raw, load_to_quarentine
 
-from pipeline.config.paths import PIPE_PROCESSED_FILES_DIR, PIPE_DUPLICATE_FILES_DIR
+from pipeline.config.paths import PIPE_PROCESSED_FILES_DIR, PIPE_DUPLICATE_FILES_DIR, PIPE_INVALID_ENTITY_DIR, PIPE_EMPTY_FILES_DIR
 
-from pathlib import Path
+from pipeline.common.exceptions.pipeline_exceptions import UnknownEntityException, DuplicateFileException, EmptyfileExcept, AllRecordsQuarantinedException, InsertRecordsFail
+
 from datetime import datetime
 
 # Diretorios
 
 path_processado = PIPE_PROCESSED_FILES_DIR
 path_duplicado  = PIPE_DUPLICATE_FILES_DIR
+path_invalid_entity = PIPE_INVALID_ENTITY_DIR
+path_empty_files = PIPE_EMPTY_FILES_DIR
 
 # Conexão com banco pdv_sales
 
 conexao_bd = get_db_connection()
 
-def etapa_raw(id_execution, json_file):
+def etapa_raw(id_execution, arquivo_original):
 
     # Inicia Pipeline
 
-    inicio_step = datetime.now()
+        inicio_step = datetime.now()
+        id_exec = id_execution
 
-    id_exec = id_execution
-    arquivo = json_file
+        path_arquivo = arquivo_original
+        nome_arquivo = file_new_name(path_arquivo, inicio_step)
 
-    # Descobre e valida a entidade do arquivo
+        # Descobre e valida a entidade do arquivo
 
-    entidade = descobrir_entidade(arquivo)
+        TABELAS_RAW = {"eventos","produtos","lojas"}
 
-    TABELAS_RAW = {"eventos","produtos","lojas"}
+        entidade = descobrir_entidade(path_arquivo)
 
-    if entidade not in TABELAS_RAW:
-        raise ValueError(f"Entidade inválida: {entidade}")
+        if entidade not in TABELAS_RAW:
 
-    # Descobre tipo de arquivo (json ou jsonl)
-    
-    if entidade == "eventos":
-        tipo_file = "jsonl"
+            reject_file = rename_file(path_arquivo, datetime.now())
+            move_file(reject_file, path_invalid_entity)
 
-    else:
-        tipo_file = "json"
+            raise UnknownEntityException(arquivo=nome_arquivo, status='INTERROMPIDO',motivo='ENTIDADE_INVALIDA',
+                                          mensagem=f"a entidade não é suportada")
 
-    # extrai informações 
-               
-    tamanho_bytes   = arquivo.stat().st_size
-    hash_arquivo    = gerar_hash(arquivo)
+        reg_new_step(conexao_bd, id_exec, 'RAW', inicio_step, entidade)
 
-    # Valida arquivo
-
-    sh_file = find_hash(conexao_bd, hash_arquivo)
-
-    
-    # Aualiza tabela de auditoria, caso hash já exista no banco
-
-    if sh_file:
-
-        fim_step = datetime.now()
-
-        upd_exec = update_execution(conexao_bd, id_exec, fim_step, "DUPLICADO", 
-                                       mensagem="Hash já processado anteriormente")
+        # Descobre tipo de arquivo (json ou jsonl)
         
-        upd_id_exec = upd_watermark_exec(conexao_bd, id_exec, fim_step)
-        conexao_bd.commit()
+        if entidade == "eventos":
+            tipo_file = "jsonl"
 
-        if entidade != "eventos":
-
-            arquivo = rename_file(arquivo, inicio_step)
         else:
-            arquivo
+            tipo_file = "json"
 
-        nome_arquivo = Path(arquivo).name
+        # extrai informações 
+                
+        tamanho_bytes   = path_arquivo.stat().st_size
+        hash_arquivo    = gerar_hash(path_arquivo)
 
-        # Move arquivo duplicado para pasta arq_duplicados
+        # Valida arquivo
 
-        move_file(arquivo, path_duplicado)
+        sh_file = find_hash(conexao_bd, hash_arquivo)
 
-        return 'DUPLICADO', 'None', nome_arquivo, hash_arquivo, entidade
+        # Aualiza tabela de auditoria, caso hash já exista no banco
 
+        if sh_file:
 
-    # Registra novo arquivo na tabela de auditoria.
+            fim_step = datetime.now()
+            
+            if entidade != "eventos":
+                path_arquivo = rename_file(path_arquivo, inicio_step)
 
-    else:
+            else:
+                path_arquivo
 
-        registros = extrair_registros(arquivo, tipo_file)
+            move_file(path_arquivo, path_duplicado)
 
-        lote = []
-        tam_lote = 1000
-        linhas_lidas = 0
-        linhas_gravadas = 0
+            raise DuplicateFileException(arquivo=nome_arquivo, status='INTERROMPIDO', motivo='ARQUIVO_DUPLICADO', 
+                                         mensagem=f"o arquivo '{nome_arquivo}' já foi processado anteriormente.")
+
+        else:
+
+            lote = []
+            tam_lote = 1000
+            linhas_lidas = 0
+            linhas_gravadas = 0
+            
+        if is_file_empty(path_arquivo):
+
+            path_arquivo = rename_file(path_arquivo, inicio_step)
+            move_file(path_arquivo, path_empty_files)
+
+            raise EmptyfileExcept(arquivo=nome_arquivo, status="INTERROMPIDO" , motivo="ARQUIVO_VAZIO",
+                                    mensagem=f"o arquivo '{nome_arquivo}' esta vazio.")
         
+        # Extrai registros do arquivo
 
-        # Verifica schema e faz o load na entidade
-        
+
+        registros = extrair_registros(path_arquivo, tipo_file)
+
         try:
-
-            nome_arquivo = Path(arquivo).name
 
             for linha in registros:
                 
@@ -111,17 +112,11 @@ def etapa_raw(id_execution, json_file):
 
                 # Valida schema da linha
 
-                try:
-
-                     version_schema, status, validation_error = validate_schema(linha, entidade)
-
-                except Exception as e:
-
-                    print(f"erro: {e}")
+                version_schema, status, validation_error = validate_schema(linha, entidade)
 
                 # Verifica status do schema_validation
         
-                if not status == "VALIDO":
+                if status == "INVALIDO":
                         
                     load_to_quarentine(
                         conexao_bd,
@@ -132,36 +127,39 @@ def etapa_raw(id_execution, json_file):
                         status,
                         validation_error)
                     
-
                     nome_entidade = f"raw.{entidade}"
                     load_to_raw(conexao_bd, nome_arquivo, [linha], nome_entidade, version_schema,
                                 'INVALIDO', 'Em quarentena')
                     
-                else:       
+                elif status == "VALIDO":      
 
                     lote.append(linha)
 
                     # Carrega lote na tabela raw.
-            
+
+                load_tentativa_1 = 0 
+
                 if len(lote) == tam_lote:
+
+                    load_tentativa_1 += 1
                     
-                    load = load_to_raw(conexao_bd, nome_arquivo, lote, entidade, version_schema,
-                                        'VALIDO', None)
-                    #conexao_bd.commit()
+                    load = load_to_raw(conexao_bd, nome_arquivo, lote, entidade, version_schema,'VALIDO', None)
                     linhas_gravadas += len(lote)                            
                     lote.clear()
 
+            load_tentativa_2 = 0 
+
             if lote:
+                load_tentativa_2 += 1
 
-                load_to_raw(conexao_bd, nome_arquivo, lote, entidade, version_schema,
-                            'VALIDO', None)
-
-                #conexao_bd.commit()
-        
+                load_to_raw(conexao_bd, nome_arquivo, lote, entidade, version_schema,'VALIDO', None)
                 linhas_gravadas += len(lote) 
-
                 lote.clear()
-        
+                
+                if (load_tentativa_1 + load_tentativa_2) == 0:
+                    raise AllRecordsQuarantinedException(arquivo=nome_arquivo, status="INTERROMPIDO", motivo="REGISTROS_INVALIDOS",
+                                                            mensagem="todos os registros estão em quarentena")
+                
             if not lote:
                 
                 # Registra arquivo na tabela de auditoria (audit.file_history)
@@ -175,26 +173,30 @@ def etapa_raw(id_execution, json_file):
                 diferenca = (fim_step - inicio_step)
                 duracao   = diferenca.total_seconds()
 
-                conexao_bd.commit()
-
                 # Atualiza tabela audit.pipeline_step
 
-                reg_new_step(conexao_bd, id_exec, id_arquivo, 'RAW', entidade,
-                            linhas_lidas, linhas_gravadas, inicio_step, fim_step,
-                                duracao)
-                
+                upd_step(conexao_bd, id_exec, 'RAW', fim_step, 'SUCESSO', duracao, id_arquivo, entidade, linhas_lidas, linhas_gravadas)
+
                 conexao_bd.commit()
 
-                # Move arquivos processados e finaliza a etapa de ingestão.
+        except Exception as e:
 
-                move = move_file(arquivo, path_processado)
-
-                if move:
-                    
-                    delete_empty_dir(arquivo)
-
-                    return 'SUCESSO', id_arquivo, nome_arquivo, hash_arquivo, entidade
-        
-        except Exception:
             conexao_bd.rollback()
-            raise
+
+            raise InsertRecordsFail(arquivo=nome_arquivo, status="INTERROMPIDO", motivo="FALHA_NO_LOAD_RAW",
+                                    mensagem=f"erro: {e}")
+
+                                # Move arquivos processados e finaliza a etapa de ingestão.
+
+        if entidade != "eventos":
+
+            path_arquivo = rename_file(path_arquivo, inicio_step)
+        else:
+            path_arquivo
+
+        move = move_file(path_arquivo, path_processado)
+
+        if move:
+            delete_empty_dir(path_arquivo)
+
+        return  id_arquivo, nome_arquivo, entidade
